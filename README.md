0b71da1409fdd63fa71fa1f51d78edb59e61bd99
Venkat <srivenkatsairaj13@gmail.com>
2024-07-25 20:15:44 +0530

    Grouping of Tests

--- README.md
+++ README.md
@@ -12,8 +12,17 @@ Jest comes with a wide range of testing and assertion utilities that help develo
  React Testing Library is specifically tailored to test components in the React ecosystem
 
 While Jest is a Test Runner and React Testing Library is specific to react ecosystem.
-![alt text](image.png)
+
+<img  src='./image.png' height='360px' width='380px' />
 
 3. Anatomy of a test
 4. Writing you first Test 
 5. TDD (Test Driven Development)
+6. Jest Watch Mode
+7. Filtering Tests
+    * filtering with file name and test name
+    * .only and .skip
+8. Grouping Tests
+    * describe('name',test fns)
+    * can be nested
+    * has .only and .skip
--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
 
-test("Greet Component Rendered", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
-});
+describe("Greet", () => {
+  test(" Component Rendered", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("Greet Render with name", () => {
-  render(<Greet name="venkat" />);
-  const textElement = screen.getByText("Hello venkat");
-  expect(textElement).toBeInTheDocument();
+  test("Render with name", () => {
+    render(<Greet name="venkat" />);
+    const textElement = screen.getByText("Hello venkat");
+    expect(textElement).toBeInTheDocument();
+  });
 });
